93c7ae936d1cb791f6bc42ca5c5aed1fb506b6c3
artem <artem98.tema@gmail.com>
2019-10-09 23:21:57 +0300

    programe1

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 'use strict';
-console.log(some);
-let evenWeek = {
-    monday: 'some',
-
+let money = prompt("Ваш бюджет на месяц?","");
+let time = prompt("Введите дату в формате YYYY-MM-DD","");
+let appData = {
+    budget:money,
+    timeData:time,
+    expenses:{},
+    optionalExpenses:{},
+    income:[],
+    savings:false
 };
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
 
-let some =1;
-var sume = 2;
-const pi = 3.14;
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
